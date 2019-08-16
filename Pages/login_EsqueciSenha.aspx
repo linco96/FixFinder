15992be61cf198d66c9c29316bfb8de7accf214a
thome <thome@MATHEUS-PC>
2019-08-15 22:46:17 -0300

    Concluído: Esqueci minha senha.

--- Pages/login_EsqueciSenha.aspx
+++ Pages/login_EsqueciSenha.aspx
@@ -1,22 +1,34 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login_EsqueciSenha.aspx.cs" Inherits="FixFinder.Pages.login_EsqueciSenha" %>
 
+<%@ Register TagPrefix="uc" TagName="Header_Control" Src="~/Controls/Header_Control.ascx" %>
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title></title>
+    <title>Esqueci minha senha</title>
     <link href="../Content/bootstrap.min.css" rel="stylesheet" />
     <script src="../Scripts/bootstrap.min.js"></script>
     <script src="../Scripts/jquery-3.4.1.min.js"></script>
     <script src="../Scripts/popper.min.js"></script>
+    <script src="../Scripts/jquery.mask.js"></script>
 </head>
 <body>
     <form id="form_EsqueceuSenha" runat="server">
+        <uc:Header_Control runat="server" ID="Header_Control"></uc:Header_Control>
+        <h1 style="text-align: center" class="display-4 text-primary">Fix Finder</h1>
         <div class="container w-25 p-3">
             <div class="form-group">
-                <h1 style="text-align: center" class="display-4 text-primary">Digite seu CPF e ou e-mail</h1>
-                <asp:TextBox runat="server" ID="txt_CPF" class="form-control" aria-describedby="emailHelp" placeholder="Digite seu CPF" required></asp:TextBox>
+                <label>Forneça seu</label>
+                <br />
+                <label class="font-weight-bold text-primary">CPF</label>
+                <asp:TextBox runat="server" ID="txt_CPF" minlength="14" onkeypress="$(this).mask('000.000.000-00');" class="form-control" placeholder="Digite seu CPF"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label>Ou</label>
+                <br />
+                <label class="font-weight-bold text-primary">E-mail</label>
+                <asp:TextBox runat="server" ID="txt_Email" CssClass="form-control" aria-describedby="emailHelp" placeholder="Digite seu E-Mail"></asp:TextBox>
             </div>
             <div class="form-group">
                 <asp:Button runat="server" ID="btn_EnviarEmail" Text="Solicitar senha" OnClick="btn_EnviarEmail_Click" class="btn btn-primary" />
--- Pages/login_EsqueciSenha.aspx.cs
+++ Pages/login_EsqueciSenha.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using FixFinder.Models;
 using System.Windows;
 using System.Net.Mail;
+using FixFinder.Controls;
 
 namespace FixFinder.Pages
 {
@@ -15,53 +16,103 @@ namespace FixFinder.Pages
         protected void Page_Load(object sender, EventArgs e)
         {
             pnl_Alert.Visible = false;
+            Header_Control h = new Header_Control();
         }
 
         protected void btn_EnviarEmail_Click(object sender, EventArgs e)
         {
-            String email;
-            using (var context = new DatabaseEntities())
+            Boolean erroEmailCPF = false;
+            Cliente cliente = null;
+            String email = "";
+            String CPF = "";
+
+            pnl_Alert.Visible = false;
+
+            if (txt_CPF.Text != "")
+                CPF = txt_CPF.Text.Replace(".", "").Replace("-", "");
+
+            if (txt_Email.Text != "")
+                email = txt_Email.Text;
+
+            if (CPF == "" && email == "")
             {
-                try
+                pnl_Alert.CssClass = "alert alert-danger";
+                lbl_Alert.Text = "Forneça um e-mail ou CPF";
+                pnl_Alert.Visible = true;
+            }
+            else
+            {
+                using (var context = new DatabaseEntities())
                 {
-                    Cliente cliente = context.Cliente.Where(c => (c.cpf == txt_CPF.Text)).FirstOrDefault<Cliente>();
-
-                    if (cliente != null)
+                    try
                     {
-                        MailMessage mail = new MailMessage();
-
-                        mail.From = new MailAddress("noreply.fixfinder@gmail.com");
-                        mail.To.Add(cliente.email);
-                        mail.Subject = "Recuperação de Senha - Fix Finder";
-                        mail.IsBodyHtml = true;
-                        string htmlBody;
-                        htmlBody = "Sua senha é " + cliente.senha;
-                        mail.Body = htmlBody;
-                        mail.Priority = MailPriority.Normal;
-                        mail.SubjectEncoding = System.Text.Encoding.UTF8;
-                        mail.BodyEncoding = System.Text.Encoding.UTF8;
-
-                        SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-                        smtp.EnableSsl = true;
-                        smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                        smtp.UseDefaultCredentials = false;
-                        smtp.Credentials = new System.Net.NetworkCredential("noreply.fixfinder@gmail.com", "tccfixfinder");
-                        smtp.Send(mail);
-
-                        pnl_Alert.CssClass = "alert alert-success";
-                        lbl_Alert.Text = "E-mail enviado.";
-                        pnl_Alert.Visible = true;
+                        if (CPF != "" && email == "")
+                            cliente = context.Cliente.Where(c => (c.cpf == CPF)).FirstOrDefault<Cliente>();
+                        else if (email != "" && CPF == "")
+                            cliente = context.Cliente.Where(c => (c.email == email)).FirstOrDefault<Cliente>();
+                        else
+                        {
+                            if (context.Cliente.Where(c => (c.email == email)).FirstOrDefault<Cliente>() != context.Cliente.Where(c => (c.cpf == CPF)).FirstOrDefault<Cliente>())
+                            {
+                                pnl_Alert.CssClass = "alert alert-danger";
+                                lbl_Alert.Text = "O e-mail informado não corresponde ao CPF cadastrado. Forneça apenas um dos campos e tente novamente";
+                                pnl_Alert.Visible = true;
+                                erroEmailCPF = true;
+                            }
+                            else
+                            {
+                                cliente = context.Cliente.Where(c => (c.cpf == CPF)).FirstOrDefault<Cliente>();
+                            }
+                        }
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        lbl_Alert.Text = "CPF/E-mail não cadastrado!";
-                        pnl_Alert.Visible = true;
+                        Response.Write("<script>alert('" + ex.Message + "');</script>");
                     }
                 }
-                catch (Exception ex)
+                if (cliente != null)
                 {
-                    Response.Write("<script>alert('" + ex.Message + "');</script>");
+                    Enviar_Email(cliente);
+                    pnl_Alert.CssClass = "alert alert-success";
+                    lbl_Alert.Text = "E-mail enviado.";
+                    pnl_Alert.Visible = true;
                 }
+                else if (!erroEmailCPF)
+                {
+                    pnl_Alert.CssClass = "alert alert-danger";
+                    lbl_Alert.Text = "CPF/E-mail não cadastrado!";
+                    pnl_Alert.Visible = true;
+                }
+            }
+        }
+
+        protected void Enviar_Email(Cliente cliente)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+
+                mail.From = new MailAddress("noreply.fixfinder@gmail.com");
+                mail.To.Add(cliente.email);
+                mail.Subject = "Recuperação de Senha - Fix Finder";
+                mail.IsBodyHtml = true;
+                string htmlBody;
+                htmlBody = "Olá <label style='font-weight: bold; color: #1E90FF'>" + cliente.nome + "</label>,<br /> Conforme solicitado, segue sua senha de acesso ao Fix Finder:<br /><br />Senha: " + cliente.senha + "<br /><br />Agora que voçê já lembrou sua senha de acesso, <br />pode consultar seus agendamento e acompanhar seus orçamentos normalmente.<br /><br />Equipe FixFinder agradece.";
+                mail.Body = htmlBody;
+                mail.Priority = MailPriority.Normal;
+                mail.SubjectEncoding = System.Text.Encoding.UTF8;
+                mail.BodyEncoding = System.Text.Encoding.UTF8;
+
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("noreply.fixfinder@gmail.com", "tccfixfinder");
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
             }
         }
     }

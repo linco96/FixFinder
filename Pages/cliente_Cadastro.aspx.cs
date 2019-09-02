4aad48788935cac6222485cdcd96b6e745e925be
Matheus <Matheus@DESKTOP-JQVBSRB>
2019-09-02 19:48:40 -0300

    uh

--- Pages/cliente_Cadastro.aspx.cs
+++ Pages/cliente_Cadastro.aspx.cs
@@ -64,8 +64,6 @@ namespace FixFinder.Pages
                             pnl_Alert.Visible = true;
                             pnl_Alert.CssClass = "alert alert-success";
                             lbl_Alert.Text = "Usuário cadastrado com sucesso";
-
-                            Response.Redirect("login.aspx", false);
                         }
                     }
                 }

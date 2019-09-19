8a2dbe50d421bd4300293e3263ca7187e9d2f3f3
Lincoln <lincolnvendramel890@gmail.com>
2019-09-19 20:02:26 -0300

    fiz uns ch-ch-ch-ch

--- Pages/funcionario_Requisicoes.aspx.cs
+++ Pages/funcionario_Requisicoes.aspx.cs
@@ -12,6 +12,11 @@ namespace FixFinder.Pages
     public partial class funcionario_Requisicoes : System.Web.UI.Page
     {
         private Cliente c;
+        private static List<Button> aceitarButtons;
+        private static List<Button> rejeitarButtons;
+        private static List<Label> labels;
+        private static List<Button> confirmarButtons;
+        private static List<Button> recusarButtons;
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -23,6 +28,26 @@ namespace FixFinder.Pages
             else
             {
                 div_Cards.InnerHtml = "";
+                if (aceitarButtons == null)
+                    aceitarButtons = new List<Button>();
+                else
+                    aceitarButtons.Clear();
+                if (rejeitarButtons == null)
+                    rejeitarButtons = new List<Button>();
+                else
+                    rejeitarButtons.Clear();
+                if (labels == null)
+                    labels = new List<Label>();
+                else
+                    labels.Clear();
+                if (confirmarButtons == null)
+                    confirmarButtons = new List<Button>();
+                else
+                    confirmarButtons.Clear();
+                if (recusarButtons == null)
+                    recusarButtons = new List<Button>();
+                else
+                    recusarButtons.Clear();
                 carregar_Requisicoes();
                 using (DatabaseEntities context = new DatabaseEntities())
                 {
@@ -84,9 +109,9 @@ namespace FixFinder.Pages
                     HtmlGenericControl nomeOficina;
                     HtmlGenericControl cargo;
                     HtmlGenericControl salario;
-                    Label dadosBancarios;
-                    Button btn_Aceitar;
-                    Button btn_Rejeitar;
+                    HtmlGenericControl hr;
+                    Label label;
+                    Button btn;
                     foreach (RequisicaoFuncionario req in requisicoes)
                     {
                         card = new Panel();
@@ -113,27 +138,65 @@ namespace FixFinder.Pages
                         salario.Attributes.Add("class", "card-title");
                         salario.InnerHtml = "Salário: R$ " + req.salario;
 
-                        dadosBancarios = new Label();
-                        dadosBancarios.CssClass = "card-text";
-                        dadosBancarios.Text = "Dados bancários:<br />Banco: " + req.banco + "<br />Agência: " + req.agencia + "<br />Conta: " + req.conta + "<br />";
+                        label = new Label();
+                        label.CssClass = "card-text";
+                        label.Text = "Dados bancários:<br />Banco: " + req.banco + "<br />Agência: " + req.agencia + "<br />Conta: " + req.conta + "<br />";
 
-                        btn_Aceitar = new Button();
-                        btn_Aceitar.Click += new EventHandler(btn_Aceitar_Click);
-                        btn_Aceitar.Text = "Aceitar";
-                        btn_Aceitar.CssClass = "btn btn-primary mr-1 mt-3";
-                        btn_Aceitar.CommandArgument = req.Oficina.cnpj;
-
-                        btn_Rejeitar = new Button();
-                        btn_Rejeitar.Click += new EventHandler(btn_Rejeitar_Click);
-                        btn_Rejeitar.Text = "Rejeitar";
-                        btn_Rejeitar.CssClass = "btn btn-danger ml-1 mt-3";
-                        btn_Rejeitar.CommandArgument = req.Oficina.cnpj;
+                        hr = new HtmlGenericControl("hr");
+                        hr.Attributes.Add("class", "border-dark mt-3 mb-3");
 
                         body.Controls.Add(cargo);
                         body.Controls.Add(salario);
-                        body.Controls.Add(dadosBancarios);
-                        body.Controls.Add(btn_Aceitar);
-                        body.Controls.Add(btn_Rejeitar);
+                        body.Controls.Add(label);
+                        body.Controls.Add(hr);
+
+                        btn = new Button();
+                        btn.Click += new EventHandler(btn_Confirmacao_Click);
+                        btn.ID = "btn_Aceitar" + req.cnpjOficina;
+                        btn.Text = "Aceitar";
+                        btn.CssClass = "btn btn-success mr-1";
+                        btn.CommandArgument = req.Oficina.cnpj;
+                        btn.CommandName = "Aceite";
+                        body.Controls.Add(btn);
+                        aceitarButtons.Add(btn);
+
+                        btn = new Button();
+                        btn.Click += new EventHandler(btn_Confirmacao_Click);
+                        btn.ID = "btn_Rejeitar" + req.cnpjOficina;
+                        btn.Text = "Rejeitar";
+                        btn.CssClass = "btn btn-danger ml-1";
+                        btn.CommandArgument = req.Oficina.cnpj;
+                        btn.CommandName = "Rejeite";
+                        body.Controls.Add(btn);
+                        rejeitarButtons.Add(btn);
+
+                        label = new Label();
+                        label.CssClass = "card-text font-weight-bold";
+                        label.ID = "lbl_" + req.cnpjOficina;
+                        label.Text = "Teu cu";
+                        label.Visible = false;
+                        body.Controls.Add(label);
+                        labels.Add(label);
+
+                        btn = new Button();
+                        btn.Click += new EventHandler(btn_Confirmar_Click);
+                        btn.ID = "btn_Confirmar" + req.cnpjOficina;
+                        btn.Text = "Sim";
+                        btn.CssClass = "btn btn-success mr-1 mt-3";
+                        btn.CommandArgument = req.Oficina.cnpj;
+                        btn.Visible = false;
+                        body.Controls.Add(btn);
+                        confirmarButtons.Add(btn);
+
+                        btn = new Button();
+                        btn.Click += new EventHandler(btn_Recusar_Click);
+                        btn.ID = "btn_Recusar" + req.cnpjOficina;
+                        btn.Text = "Não";
+                        btn.CssClass = "btn btn-danger ml-1 mt-3";
+                        btn.CommandArgument = req.Oficina.cnpj;
+                        btn.Visible = false;
+                        body.Controls.Add(btn);
+                        recusarButtons.Add(btn);
 
                         card.Controls.Add(body);
                         div_Cards.Controls.Add(card);
@@ -148,18 +211,180 @@ namespace FixFinder.Pages
             }
         }
 
-        protected void btn_Aceitar_Click(object sender, EventArgs e)
+        protected void btn_Confirmacao_Click(object sender, EventArgs e)
+        {
+            Button origin = sender as Button;
+            try
+            {
+                Button btn_Aceitar = new Button(), btn_Rejeitar = new Button(), btn_Confirmar = new Button(), btn_Recusar = new Button();
+                Label label = new Label();
+                foreach (Button btn in aceitarButtons)
+                {
+                    if (btn.CommandArgument.Equals(origin.CommandArgument))
+                    {
+                        btn_Aceitar = btn;
+                        break;
+                    }
+                }
+                foreach (Button btn in rejeitarButtons)
+                {
+                    if (btn.CommandArgument.Equals(origin.CommandArgument))
+                    {
+                        btn_Rejeitar = btn;
+                        break;
+                    }
+                }
+                foreach (Button btn in confirmarButtons)
+                {
+                    if (btn.CommandArgument.Equals(origin.CommandArgument))
+                    {
+                        btn_Confirmar = btn;
+                        break;
+                    }
+                }
+                foreach (Button btn in recusarButtons)
+                {
+                    if (btn.CommandArgument.Equals(origin.CommandArgument))
+                    {
+                        btn_Recusar = btn;
+                        break;
+                    }
+                }
+                foreach (Label lbl in labels)
+                {
+                    if (lbl.ID.Split('_')[1].Equals(origin.CommandArgument))
+                    {
+                        label = lbl;
+                        break;
+                    }
+                }
+
+                btn_Aceitar.Visible = false;
+                btn_Rejeitar.Visible = false;
+
+                label.Visible = true;
+
+                btn_Confirmar.Visible = true;
+                btn_Recusar.Visible = true;
+
+                if (origin.CommandName.Equals("Aceite"))
+                {
+                    btn_Confirmar.CommandName = "Aceite";
+                    btn_Recusar.CommandName = "Aceite";
+                    label.Text = "Tem certeza que deseja confirmar esta requisição?<br />";
+                }
+                else
+                {
+                    btn_Confirmar.CommandName = "Rejeite";
+                    btn_Recusar.CommandName = "Rejeite";
+                    label.Text = "Tem certeza que deseja rejeitar esta requisição?<br />";
+                }
+            }
+            catch (Exception ex)
+            {
+                pnl_Alert.CssClass = "alert alert-danger";
+                lbl_Alert.Text = "Erro: " + ex.Message + Environment.NewLine + "Por favor entre em contato com o suporte";
+                pnl_Alert.Visible = true;
+            }
+        }
+
+        protected void btn_Confirmar_Click(object sender, EventArgs e)
+        {
+            Button origin = sender as Button;
+            try
+            {
+                if (origin.CommandName.Equals("Aceite"))
+                {
+                    aceitarRequisicao(origin.CommandArgument);
+                }
+                else
+                {
+                    rejeitarRequisicao(origin.CommandArgument);
+                }
+            }
+            catch (Exception ex)
+            {
+                pnl_Alert.CssClass = "alert alert-danger";
+                lbl_Alert.Text = "Erro: " + ex.Message + Environment.NewLine + "Por favor entre em contato com o suporte";
+                pnl_Alert.Visible = true;
+            }
+        }
+
+        protected void btn_Recusar_Click(object sender, EventArgs e)
+        {
+            Button origin = sender as Button;
+            try
+            {
+                Button btn_Aceitar = new Button(), btn_Rejeitar = new Button(), btn_Confirmar = new Button(), btn_Recusar = new Button();
+                Label label = new Label();
+                foreach (Button btn in aceitarButtons)
+                {
+                    if (btn.CommandArgument.Equals(origin.CommandArgument))
+                    {
+                        btn_Aceitar = btn;
+                        break;
+                    }
+                }
+                foreach (Button btn in rejeitarButtons)
+                {
+                    if (btn.CommandArgument.Equals(origin.CommandArgument))
+                    {
+                        btn_Rejeitar = btn;
+                        break;
+                    }
+                }
+                foreach (Button btn in confirmarButtons)
+                {
+                    if (btn.CommandArgument.Equals(origin.CommandArgument))
+                    {
+                        btn_Confirmar = btn;
+                        break;
+                    }
+                }
+                foreach (Button btn in recusarButtons)
+                {
+                    if (btn.CommandArgument.Equals(origin.CommandArgument))
+                    {
+                        btn_Recusar = btn;
+                        break;
+                    }
+                }
+                foreach (Label lbl in labels)
+                {
+                    if (lbl.ID.Split('_')[1].Equals(origin.CommandArgument))
+                    {
+                        label = lbl;
+                        break;
+                    }
+                }
+
+                btn_Aceitar.Visible = true;
+                btn_Rejeitar.Visible = true;
+
+                label.Visible = false;
+
+                btn_Confirmar.Visible = false;
+                btn_Recusar.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                pnl_Alert.CssClass = "alert alert-danger";
+                lbl_Alert.Text = "Erro: " + ex.Message + Environment.NewLine + "Por favor entre em contato com o suporte";
+                pnl_Alert.Visible = true;
+            }
+        }
+
+        protected void aceitarRequisicao(string cnpj)
         {
             try
             {
-                Button btn = sender as Button;
                 using (DatabaseEntities context = new DatabaseEntities())
                 {
-                    RequisicaoFuncionario req = context.RequisicaoFuncionario.Where(requisicao => requisicao.cpfCliente == c.cpf && requisicao.cnpjOficina == btn.CommandArgument).FirstOrDefault();
+                    RequisicaoFuncionario req = context.RequisicaoFuncionario.Where(requisicao => requisicao.cpfCliente == c.cpf && requisicao.cnpjOficina == cnpj).FirstOrDefault();
                     Funcionario funcionario = new Funcionario
                     {
                         cpf = c.cpf,
-                        cnpjOficina = btn.CommandArgument,
+                        cnpjOficina = cnpj,
                         cargo = req.cargo,
                         salario = req.salario,
                         banco = req.banco,
@@ -186,14 +411,13 @@ namespace FixFinder.Pages
             }
         }
 
-        protected void btn_Rejeitar_Click(object sender, EventArgs e)
+        protected void rejeitarRequisicao(string cnpj)
         {
             try
             {
-                Button btn = sender as Button;
                 using (DatabaseEntities context = new DatabaseEntities())
                 {
-                    RequisicaoFuncionario req = context.RequisicaoFuncionario.Where(requisicao => requisicao.cpfCliente == c.cpf && requisicao.cnpjOficina == btn.CommandArgument).FirstOrDefault();
+                    RequisicaoFuncionario req = context.RequisicaoFuncionario.Where(requisicao => requisicao.cpfCliente == c.cpf && requisicao.cnpjOficina == cnpj).FirstOrDefault();
                     context.RequisicaoFuncionario.Remove(req);
                     context.SaveChanges();
 

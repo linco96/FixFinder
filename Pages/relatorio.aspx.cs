8e57fa7b85d59ffecce9173463674c1422334dc9
thome <thome@MATHEUS-PC>
2019-10-26 17:19:55 -0300

    validacao de datas

--- Pages/relatorio.aspx.cs
+++ Pages/relatorio.aspx.cs
@@ -25,42 +25,43 @@ namespace FixFinder.Pages
                 using (DatabaseEntities context = new DatabaseEntities())
                 {
                     f = context.Funcionario.Where(func => func.cpf.Equals(c.cpf)).FirstOrDefault();
-                    if (f != null && f.cargo.ToLower().Equals("gerente"))
+                    lbl_Nome.Text = c.nome;
+                    if (f == null)
                     {
-                        lbl_Nome.Text = c.nome;
-                        if (f == null)
-                        {
-                            pnl_Oficina.Visible = false;
-                            btn_CadastroOficina.Visible = true;
+                        pnl_Oficina.Visible = false;
+                        btn_CadastroOficina.Visible = true;
 
-                            List<RequisicaoFuncionario> requisicoes = context.RequisicaoFuncionario.Where(r => r.cpfCliente.Equals(c.cpf)).ToList();
-                            if (requisicoes.Count > 0)
-                            {
-                                pnl_Funcionario.Visible = true;
-                                badge_Requisicoes.InnerHtml = requisicoes.Count.ToString();
-                            }
-                            else
-                            {
-                                pnl_Funcionario.Visible = false;
-                            }
+                        List<RequisicaoFuncionario> requisicoes = context.RequisicaoFuncionario.Where(r => r.cpfCliente.Equals(c.cpf)).ToList();
+                        if (requisicoes.Count > 0)
+                        {
+                            pnl_Funcionario.Visible = true;
+                            badge_Requisicoes.InnerHtml = requisicoes.Count.ToString();
                         }
                         else
                         {
-                            pnl_Oficina.Visible = true;
                             pnl_Funcionario.Visible = false;
-                            btn_CadastroOficina.Visible = false;
-                            lbl_Nome.Text += " | " + f.Oficina.nome;
-                            if (f.cargo.ToLower().Equals("gerente"))
-                            {
-                                btn_Configuracoes.Visible = true;
-                                btn_Funcionarios.Visible = true;
-                            }
-                            else
-                            {
-                                btn_Configuracoes.Visible = false;
-                                btn_Funcionarios.Visible = false;
-                            }
                         }
+                    }
+                    else
+                    {
+                        pnl_Oficina.Visible = true;
+                        pnl_Funcionario.Visible = false;
+                        btn_CadastroOficina.Visible = false;
+                        lbl_Nome.Text += " | " + f.Oficina.nome;
+                        if (f.cargo.ToLower().Equals("gerente"))
+                        {
+                            btn_Configuracoes.Visible = true;
+                            btn_Funcionarios.Visible = true;
+                        }
+                        else
+                        {
+                            btn_Configuracoes.Visible = false;
+                            btn_Funcionarios.Visible = false;
+                        }
+                    }
+                    if (f != null && f.cargo.ToLower().Equals("gerente"))
+                    {
+                        pnl_Alert.Visible = false;
                     }
                     else
                     {
@@ -78,6 +79,39 @@ namespace FixFinder.Pages
 
         protected void btn_GerarGrafico_Click(object sender, EventArgs e)
         {
+            if (validacaoDatas())
+            {
+            }
+        }
+
+        private Boolean validacaoDatas()
+        {
+            DateTime dtInicio, dtFim;
+            dtInicio = DateTime.Parse(txt_DataInicio.Text);
+            dtFim = DateTime.Parse(txt_DataFim.Text);
+
+            if (dtInicio >= DateTime.Today)
+            {
+                pnl_Alert.Visible = true;
+                lbl_Alert.Text = "A data de início tem que ser menor do que a data de hoje";
+                return false;
+            }
+
+            if (dtFim > DateTime.Today)
+            {
+                pnl_Alert.Visible = true;
+                lbl_Alert.Text = "A data de fim tem que ser menor ou igual a data de hoje";
+                return false;
+            }
+
+            if (dtInicio >= dtFim)
+            {
+                pnl_Alert.Visible = true;
+                lbl_Alert.Text = "A data de início tem que ser menor do que a data fim";
+                return false;
+            }
+
+            return true;
         }
     }
 }
